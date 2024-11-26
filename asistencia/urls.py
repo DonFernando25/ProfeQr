2d668d6408fe7fbbe4acaca10b662dd8b19e6ef0
DonFernando25 <fe.arevaloa@duocuc.cl>
2024-11-26 20:24:26 -0300

    adad

--- asistencia/urls.py
+++ asistencia/urls.py
@@ -20,5 +20,5 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-     path('', views.index, name='index'),
+    path('generar_qr/<int:clase_id>/', views.generar_qr, name='generar_qr'),
 ]
